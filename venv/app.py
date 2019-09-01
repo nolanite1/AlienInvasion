2fb0eb9f6c4608ca55374ba5fa974c2929098cf8
Sailesh Sai Sirigineedi <sai2468@Saileshs-MacBook-Pro.local>
2019-09-01 17:26:38 -0500

    changing path of settings

--- venv/app.py
+++ venv/app.py
@@ -5,6 +5,7 @@ import sys
 def run_game():
     # Initialize a game and create a screen object
     pygame.init()
+    ai_settings = Settings()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
     bg_color = (230,230,230)
