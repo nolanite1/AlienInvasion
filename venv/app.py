3e65ef182ca41d10d308f89c50d128cda41146a2
Sailesh Sai Sirigineedi <sai2468@Saileshs-MacBook-Pro.local>
2019-09-01 17:04:56 -0500

    creating screen for game

--- venv/app.py
+++ venv/app.py
@@ -1,1 +1,17 @@
 import pygame
+
+import sys
+
+def run_game():
+    #Initalizee a game and create a screen object
+    pygame.init()
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alien Invasion")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                    sys.exit()
+        pygame.display.flip()
+
+
+run_game()
