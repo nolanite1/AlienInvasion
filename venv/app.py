da8b6d4f808bbc85826a66f20503ca969c31b26b
Sailesh Sai Sirigineedi <sai2468@Saileshs-MacBook-Pro.local>
2019-09-01 17:30:00 -0500

    changing settings to one from settings module instead of manual

--- venv/app.py
+++ venv/app.py
@@ -1,19 +1,18 @@
 import pygame
 import sys
-
+from settings import Settings
 
 def run_game():
     # Initialize a game and create a screen object
     pygame.init()
     ai_settings = Settings()
-    screen = pygame.display.set_mode((1200,800))
+    screen = pygame.display.set_mode(ai_settings.screen_width,ai_settings.screen_height)
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (230,230,230)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-        screen.fill(bg_color)
+        screen.fill(ai_settings.bg_color)
         pygame.display.flip()
 
 
