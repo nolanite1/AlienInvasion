d377ccc4f48ed399f56d21a32b031c9d8fbe0483
Sailesh Sai Sirigineedi <sai2468@Saileshs-MacBook-Pro.local>
2019-09-01 17:24:44 -0500

    adding settings module

--- venv/app.py
+++ venv/app.py
@@ -1,16 +1,18 @@
 import pygame
-
 import sys
 
+
 def run_game():
-    #Initalizee a game and create a screen object
+    # Initialize a game and create a screen object
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
+    bg_color = (230,230,230)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                    sys.exit()
+                sys.exit()
+        screen.fill(bg_color)
         pygame.display.flip()
 
 
