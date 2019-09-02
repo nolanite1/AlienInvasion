5c4e5ecaab1c70f90929d3c80318becc9797bdb6
Sailesh Sai Sirigineedi <sai2468@Saileshs-MacBook-Pro.local>
2019-09-02 16:18:07 -0500

    changing color to black

--- venv/settings.py
+++ venv/settings.py
@@ -1,6 +1,6 @@
-class Settings():
+class Settings:
 
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230,230)
+        self.bg_color = (0, 0)
