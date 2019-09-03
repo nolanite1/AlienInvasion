a8421bf227b79ed824a739275ecec08bcd39bd39
Sailesh Sai Sirigineedi <sai2468@Saileshs-MacBook-Pro.local>
2019-09-02 20:41:51 -0500

    refactoring updating screen to seperate module

--- venv/app.py
+++ venv/app.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 def run_game():
     # Initialize a game and create a screen object
     pygame.init()
@@ -10,12 +11,8 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     ship = Ship(screen)
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings,ship,screen)
 
 
 run_game()
--- venv/game_functions.py
+++ venv/game_functions.py
@@ -1,8 +1,15 @@
 import sys
 import pygame
+
+
 def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+def update_screen(ai_settings,ship,screen):
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    pygame.display.flip()
 
 
