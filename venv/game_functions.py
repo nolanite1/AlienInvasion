bcaf9e330daee0fa0ae2f71617edc14e44ddd569
Sailesh Sai Sirigineedi <sai2468@saileshs-mbp.ddns.uark.edu>
2019-09-04 20:11:10 -0500

    adding movement for ship

--- venv/game_functions.py
+++ venv/game_functions.py
@@ -2,10 +2,16 @@ import sys
 import pygame
 
 
-def check_events():
+def check_events(ship):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                ship.rect.centerx+=5
+            elif event.key == pygame.K_LEFT:
+                ship.rect.centerx-=5
+
 
 def update_screen(ai_settings,ship,screen):
     screen.fill(ai_settings.bg_color)
