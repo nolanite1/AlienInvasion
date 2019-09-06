c088b5eb12725dd83d616e6c6dd776489314e07e
Sailesh Sai Sirigineedi <sai2468@saileshs-mbp.ddns.uark.edu>
2019-09-05 19:37:14 -0500

    adjusting speed of ship so it doesn't move one pixel at a time

--- venv/ship.py
+++ venv/ship.py
@@ -1,20 +1,22 @@
 import pygame
 
 class Ship():
-    def __init__(self, screen):
+    def __init__(self, ai_settings,screen):
+        self.ai_settings = ai_settings
         self.screen = screen
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
+        self.center = float(self.rect.centerx)
         self.rect.bottom = self.screen_rect.bottom
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         if self.moving_right:
-            self.rect.centerx+=1
+            self.rect.centerx+=self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx-=1
+            self.rect.centerx-=self.ai_settings.ship_speed_factor
     def blitme(self):
         self.screen.blit(self.image, self.rect)
